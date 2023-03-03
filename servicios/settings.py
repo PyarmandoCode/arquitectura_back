d76df78e16583781851c60d0613dc1e4583636ef
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-03-03 17:12:10 -0500

    Agregando Vercel

--- servicios/settings.py
+++ servicios/settings.py
@@ -60,8 +60,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "http://127.0.0.1:4200",
     "http://127.0.0.1:8000",
-    "https://arquitecturaback-production.up.railway.app"
-    "https://arquitectura-front.vercel.app/"
+    "https://arquitecturaback-production.up.railway.app",
+    "https://arquitectura-front.vercel.app"
     
 ]
 
